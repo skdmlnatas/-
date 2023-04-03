38566363b3a94e2f17bd3e743934cf15386b7ad0
skdmlnatas <skdmlnatas@naver.com>
2023-04-03 16:01:40 +0900

    p.71

---
+++ basic_functions.py
@@ -0,0 +1,48 @@
+def naive_relu(x):
+    assert len(x.shape) == 2
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] = max(x[i, j], 0)
+    return x
+
+def naiv_add(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] += y[i, j]
+    return x
+
+def naiv_sub(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] -= y[i, j]
+    return x
+
+def naiv_prod(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] *= y[i, j]
+    return x
+
+def naiv_prod(x, y):
+    assert len(x.shape) == 2
+    assert x.shape == y.shape
+
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] /= y[i, j]
+    return x
